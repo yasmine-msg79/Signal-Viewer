470368982250ae9bbcb122bead57a0b67be5e6a2
yasmine-msg79 <111207118+yasmine-msg79@users.noreply.github.com>
2024-10-07 14:42:11 +0300

    Initial layout for channels viewer minus live graphs

--- Classes/ChannelViewer.py
+++ Classes/ChannelViewer.py
@@ -1,3 +1,9 @@
+import PyQt5
+from PyQt5 import uic
 
-class ChannelViewer:
-    pass
+
+class ChannelViewer(PyQt5.QtWidgets.QWidget):
+    def __init__(self):
+        super(ChannelViewer, self).__init__()
+        uic.loadUi(r"..\UI\channel_viewer.UI", self)
+
--- Classes/main.py
+++ Classes/main.py
@@ -1,5 +1,5 @@
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QHBoxLayout
 import ChannelViewer
 import ControlPanel
 import ReportGenerate
@@ -16,7 +16,24 @@ class MainApp(QMainWindow):
         self.signal_fetcher = SignalFetch.SignalFetch()
 
         self.setWindowTitle("Signal Viewer")
-        self.setGeometry(200, 100, 900, 600)
+        self.showMaximized()
+
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        layout = QHBoxLayout(central_widget)
+
+        #left = QWidget()
+        right = QWidget()
+        layout.addWidget(self.signal_fetcher)
+        layout.addWidget(self.channel_viewer)
+        layout.addWidget(right)
+
+        self.signal_fetcher.setFixedWidth(200)
+        right.setFixedWidth(150)
+
+        layout.setStretch(0, 1)
+        layout.setStretch(1, 3)
+        layout.setStretch(2, 1)
 
 
 if __name__ == "__main__":
