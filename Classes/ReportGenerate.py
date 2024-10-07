73dbf8c24ed333f5d977b4e523ef5b6cde2e0d83
farhaa <111386862+farha1010@users.noreply.github.com>
2024-10-07 21:32:00 +0300

    Update ReportGenerate.py

--- Classes/ReportGenerate.py
+++ Classes/ReportGenerate.py
@@ -1,3 +1,35 @@
+import sys
+import os
+import random
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QComboBox, QMessageBox
+from PyQt5.QtCore import QTimer
+from ReportLab.lib.PageSizes import letter
+from ReportLab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle, Image
+from ReportLab.lib.units import inch
+from ReportLab.lib import colors
+from ReportLab.lib.styles import getSampleStyleSheet
+class RealTimeReportGeneratorWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.setupRealTimeData()
+    def initUI(self):
+        # Layout and Widgets
+        self.layout = QVBoxLayout()
+        
+        # Label 
+        self.layout.addWidget(QLabel("Select Signal for Real-Time Report"))
 
-class ReportGenerate:
+        # Dropdown list for Signal Selection
+        self.signal_dropdown = QComboBox(self)
+        self.signal_dropdown.addItems(["ECG", "EMG", "EEG"])  # Available signals
+        self.layout.addWidget(self.signal_dropdown)
+
+        # Button to generate report
+        self.generate_button = QPushButton("Generate Report", self)
+        self.generate_button.clicked.connect(self.on_generate_report)
+        self.layout.addWidget(self.generate_button)
+
+        self.setLayout(self.layout)
+#to_be_continued
     pass
